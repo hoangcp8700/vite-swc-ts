90cf821a263ece6d5fd1300215d81ef9bda6e7af
Dam <dam@Dambidai.local>
2023-04-28 22:24:09 +0700

    init source

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'import', 'jsx-a11y', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/function-component-definition': 0,
+    'react/react-in-jsx-scope': 0,
+
+    camelcase: 'error',
+    'consistent-return': 'off',
+    'no-unused-vars': 'off',
+    'no-console': 'off',
+    'no-plusplus': 'off',
+
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        '': 'never',
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+};
