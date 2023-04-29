55c356e329ce5938fa0e3cf8c8aad440a6a0b5c0
Hoang Cong Phan <hoangcp219@gmail.com>
2023-04-29 15:22:11 +0700

    init source

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import * as path from 'path';
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
