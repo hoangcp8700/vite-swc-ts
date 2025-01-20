ba3262fd344c8e4308e908f004ce9c953de0e45e
Hoang Cong Phan <69571824+hoangcp8700@users.noreply.github.com>
2025-01-20 09:51:31 +0700

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -6,11 +6,22 @@ export default {
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production'
       ? {
-          cssnano: {},
-          //  reduce the file size is to remove any unrequired styles from the final CSS file.
-          '@fullhuman/postcss-purgecss': {
-            content: ['./dist/*.html', './src/*.?s', './src/*.?sx'], // List every file that references the classes here. See the docs for PurgeCSS about details
-          },
+        cssnano: {
+          preset: [
+            'default',
+            {
+              discardComments: {
+                removeAll: true,
+              },
+              calc: false,
+            },
+          ],
+        },
+        'postcss-preset-env': {
+          stage: 3,
+          preserve: false,
+          features: {},
+        },
         }
       : {}),
   },
